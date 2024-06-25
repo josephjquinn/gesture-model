6a8277879e817bfc196f253e2afa598ac4ded3e3
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-06-25 10:32:35 -0400

    cnn architecture

---
+++ model/cnnet.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+
+import torch.nn.functional as F
+
+
+class cnn(nn.Module):
+    def __init__(self):
+        super(cnn, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
+        x = x.view(-1, 16 * 5 * 5)  # -> n, 400
+        x = F.relu(self.fc1(x))  # -> n, 120
+        x = F.relu(self.fc2(x))  # -> n, 84
+        x = self.fc3(x)  # -> n, 10
+        return x
