02038fb5ed801c40d412669eedf232fd93eea4d0
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-07-03 13:19:44 -0400

    Transformer Encoder block

--- model/tblocks.py
+++ model/tblocks.py
@@ -77,3 +77,28 @@ class MLPBlock(nn.Module):
         x = self.layer_norm(x)
         x = self.mlp(x)
         return x
+
+
+class TransformerEncoderBlock(nn.Module):
+    def __init__(
+        self,
+        embedding_dim: int = 768,  # Hidden size D
+        num_heads: int = 12,  # Heads
+        mlp_size: int = 3072,  # MLP size
+        mlp_dropout: float = 0.1,
+        attn_dropout: float = 0,
+    ):
+        super().__init__()
+
+        self.msa_block = MultiheadSelfAttentionBlock(
+            embedding_dim=embedding_dim, num_heads=num_heads, attn_dropout=attn_dropout
+        )
+
+        self.mlp_block = MLPBlock(
+            embedding_dim=embedding_dim, mlp_size=mlp_size, dropout=mlp_dropout
+        )
+
+    def forward(self, x):
+        x = self.msa_block(x) + x
+        x = self.mlp_block(x) + x
+        return x
