d8f91975c5a3e54e3b15d95a498db84a04ff6a3b
Joseph Quinn <81782398+josephjquinn@users.noreply.github.com>
2024-07-21 12:08:08 -0400

    updated train with argparser

--- train.py
+++ train.py
@@ -1,50 +1,109 @@
+import yaml
 import torch
-from model.cnnet import cnn
 import torch.nn as nn
-from torchvision import transforms
 import util.engine
 import util.data_loader
-
-data_path = "./data/"
-num_epochs = 100
-batch_size = 100
-learning_rate = 0.01
-TRAIN_SPLIT = 0.75
-VAL_SPLIT = 1 - TRAIN_SPLIT
-
-transform = transforms.Compose(
-    [
-        transforms.Resize((32, 32)),
-        transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ]
-)
+from model.cnn import ImageCNN, LandmarkCNN
+from model.ann import ImageANN, LandmarkANN
+from model.vit import ImageViT
+import argparse
 
 
-train_loader, val_loaader, test_loader = util.data_loader.create_dataloaders(
-    "./data/", transform, 100
-)
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+    return config
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main(config_path):
+    # Load YAML config
+    config = load_config(config_path)
+
+    # Access values from config
+    data_path = config["data"]["data_path"]
+    data_type = config["data"]["data_type"]
+    batch_size = config["data"]["batch_size"]
+    device = config["device"]
+    model_name = config["hyper"]["model"]
+    epochs = config["hyper"]["epochs"]
+    save_path = config["save_path"]
+    normalize = config["data"]["normalize"]
+    center_wrist = config["data"]["center_wrist"]
+    learning_rate = config["hyper"]["learning_rate"]
+    optimizer_name = config["hyper"]["optimizer"]
+    criterion_name = config["hyper"]["criterion"]
+    image_size = config["data"]["image_size"]
+    num_classes = config["data"]["num_classes"]
+
+    if data_type == "landmark":
+        train_loader, val_loader, test_loader = (
+            util.data_loader.create_landmark_dataloaders(
+                data_path, batch_size, normalize, center_wrist
+            )
+        )
+
+    if data_type == "image":
+        train_loader, val_loader, test_loader = (
+            util.data_loader.create_image_dataloaders(
+                data_path, image_size, normalize, batch_size
+            )
+        )
+    print(model_name)
+    print(data_type)
+
+    if model_name == "ann" and data_type == "image":
+        model = ImageANN(image_size, num_classes).to(device)
+    elif model_name == "ann" and data_type == "landmark":
+        model = LandmarkANN(num_classes).to(device)
+    elif model_name == "cnn" and data_type == "image":
+        model = ImageCNN(num_classes).to(device)
+    elif model_name == "cnn" and data_type == "landmark":
+        model = LandmarkCNN(num_classes).to(device)
+    elif model_name == "vit":
+        model = ImageViT().to(device)
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    else:
+        raise ValueError("Model not found in config.")
+
+    if optimizer_name == "sgd":
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    elif optimizer_name == "adam":
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    else:
+        raise ValueError(f"Optimizer {optimizer_name} not supported.")
+
+    if criterion_name == "CrossEntropyLoss":
+        criterion = nn.CrossEntropyLoss()
+    elif criterion_name == "MSELoss":
+        criterion = nn.MSELoss()
+    else:
+        raise ValueError(f"Criterion {criterion_name} not supported.")
+
+    print("[INFO] Training the network...")
+    res = util.engine.train(
+        model=model,
+        train_dataloader=train_loader,
+        test_dataloader=test_loader,
+        val_dataloader=val_loader,
+        loss_fn=criterion,
+        optimizer=optimizer,
+        epochs=epochs,
+        device=device,
+        save_path=save_path,
+    )
 
 
-model = cnn().to(device)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Train a model using YAML configuration."
+    )
+    parser.add_argument("--config", type=str, help="Path to YAML configuration file")
 
+    args = parser.parse_args()
 
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-H = {"train_loss": [], "train_acc": [], "val_loss": [], "val_acc": []}
-print("[INFO] training the network...")
-
-
-util.engine.train(
-    model=model,
-    train_dataloader=train_loader,
-    test_dataloader=test_loader,
-    loss_fn=criterion,
-    optimizer=optimizer,
-    epochs=10,
-    device=device,
-)
+    if args.config:
+        config_path = args.config
+        main(config_path)
+    else:
+        print("Please specify a configuration file using --config flag.")
